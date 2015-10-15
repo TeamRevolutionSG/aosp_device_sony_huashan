4d4c78f4d1b1da0192e64983e9214f058b1bb85e
Adrian DC <AdrianDC@users.noreply.github.com>
2015-10-15 15:25:12 +0200

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,11 +1,13 @@
-LOCAL_PATH := $(call my-dir)
+ifeq ($(filter-out huashan,$(TARGET_DEVICE)),)
 
-ifeq ($(TARGET_DEVICE),huashan)
-    include $(call first-makefiles-under,$(LOCAL_PATH))
+    LOCAL_PATH := $(call my-dir)
+
+    include $(call all-makefiles-under,$(LOCAL_PATH))
 
     $(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wcd9310; \
         ln -sf /data/misc/audio/wcd9310_anc.bin \
         $(TARGET_OUT_ETC)/firmware/wcd9310/wcd9310_anc.bin; \
         ln -sf /data/misc/audio/mbhc.bin \
         $(TARGET_OUT_ETC)/firmware/wcd9310/wcd9310_mbhc.bin)
+
 endif
