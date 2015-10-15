6dc87eab0c3083452f3f22e76644b944a73fa4ed
Firdaus Amin <furrydaus@gmail.com>
2015-10-15 23:22:21 +0800

    Reformat to that more similar to AOSP

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
+# 
 
 # Inherit from Sony common
 include device/sony/common/BoardConfigCommon.mk
@@ -20,72 +20,40 @@ include device/sony/common/BoardConfigCommon.mk
 TARGET_SPECIFIC_HEADER_PATH += device/sony/huashan/include
 
 # Architecture
-TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_BOARD_PLATFORM := msm8960
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_VARIANT := krait
 
 TARGET_NO_BOOTLOADER := true
-TARGET_NO_RADIOIMAGE := true
-TARGET_NO_RECOVERY := false
-TARGET_NO_KERNEL := false
-
-# Kernel properties
-TARGET_KERNEL_SOURCE := kernel/sony/msm8x60
-TARGET_KERNEL_CONFIG := cm_viskan_huashan_defconfig
-
-# Platform
-TARGET_BOOTLOADER_BOARD_NAME := MSM8960
-BOARD_VENDOR_PLATFORM := viskan
 
 # Kernel information
 BOARD_KERNEL_BASE        := 0x80200000
 BOARD_KERNEL_PAGESIZE    := 2048
-BOARD_RAMDISK_OFFSET     := 0x02000000
 
-BOARD_KERNEL_BOOTIMG := true
-BOARD_CUSTOM_MKBOOTIMG := mkqcdtbootimg
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom androidboot.selinux=permissive user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 vmalloc=400M
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
 
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.selinux=permissive
-BOARD_KERNEL_CMDLINE += user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3
-BOARD_KERNEL_CMDLINE += console=ttyHSL0,115200,n8
-BOARD_KERNEL_CMDLINE += vmalloc=400M
+# Audio
+BOARD_USES_ALSA_AUDIO := true
+BOARD_USES_LEGACY_ALSA_AUDIO := true
+TARGET_USES_QCOM_COMPRESSED_AUDIO := true
+BOARD_HAVE_NEW_QCOM_CSDCLIENT := true
+BOARD_HAVE_CSD_FAST_CALL_SWITCH := true
+BOARD_USES_FLUENCE_INCALL := true
+BOARD_USES_SEPERATED_AUDIO_INPUT := true
+BOARD_USES_SEPERATED_VOICE_SPEAKER_MIC := true
 
-TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USERIMAGES_USE_F2FS := true
-
-# Partition information
-BOARD_VOLD_MAX_PARTITIONS := 26
-
-BOARD_BOOTIMAGE_PARTITION_SIZE := 0x01400000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x01400000
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1056964608
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 2147483648
-
-TARGET_RECOVERY_FSTAB := device/sony/huashan/rootdir/fstab.qcom
-RECOVERY_FSTAB_VERSION := 2
-
-# Display HAL
-USE_OPENGL_RENDERER := true
-TARGET_USES_ION := true
-TARGET_USES_C2D_COMPOSITION := true
-
-# Time
-BOARD_USES_QC_TIME_SERVICES := true
-
-# Dumpstate
-BOARD_LIB_DUMPSTATE := libdumpstate.sony
-
-# Bionic
-MALLOC_IMPL := dlmalloc
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_QCOM := true
+BLUETOOTH_HCI_USE_MCT := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/sony/huashan/bluetooth
 
 # Wifi
-BOARD_HAS_QCOM_WLAN              := true
-BOARD_WLAN_DEVICE                := qcwcn
 WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_WLAN_DEVICE                := qcwcn
 BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
 BOARD_HOSTAPD_DRIVER             := NL80211
@@ -99,7 +67,63 @@ BOARD_USE_SONY_MACUPDATE := true
 
 BOARD_HARDWARE_CLASS := device/sony/huashan/cmhw
 
+BOARD_HAS_QCOM_WLAN      := true
 BOARD_USES_QCOM_HARDWARE := true
+
+TARGET_NO_RADIOIMAGE := true
+TARGET_BOARD_PLATFORM := msm8960
+TARGET_BOOTLOADER_BOARD_NAME := MSM8960
+BOARD_VENDOR_PLATFORM := viskan
+TARGET_NO_RECOVERY := false
+TARGET_NO_KERNEL := false
+
+# Kernel properties
+TARGET_KERNEL_SOURCE := kernel/sony/msm8x60
+TARGET_KERNEL_CONFIG := cm_viskan_huashan_defconfig
+
+# Display HAL
+USE_OPENGL_RENDERER := true
+TARGET_USES_ION := true
+TARGET_USES_C2D_COMPOSITION := true
+
+# Enable dex-preoptimization to speed up first boot sequence
+ifeq ($(HOST_OS),linux)
+  ifeq ($(TARGET_BUILD_VARIANT),userdebug)
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT := true
+    endif
+  endif
+endif
+DONT_DEXPREOPT_PREBUILTS := true
+
+# Board Properties
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+BOARD_KERNEL_BOOTIMG := true
+BOARD_CUSTOM_MKBOOTIMG := mkqcdtbootimg
+
+# Partition information
+BOARD_VOLD_MAX_PARTITIONS := 26
+
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x01400000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x01400000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1056964608
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 2147483648
+
+# Fstab Partitions
+TARGET_RECOVERY_FSTAB := device/sony/huashan/rootdir/fstab.qcom
+RECOVERY_FSTAB_VERSION := 2
+
+BOARD_CHARGER_ENABLE_SUSPEND := true
+
+# Time
+BOARD_USES_QC_TIME_SERVICES := true
+
+# Dumpstate
+BOARD_LIB_DUMPSTATE := libdumpstate.sony
+
+# Bionic
+MALLOC_IMPL := dlmalloc
 
 # Camera
 TARGET_PROVIDES_CAMERA_HAL := true
@@ -124,12 +148,6 @@ SOMC_CFG_SENSORS_LIGHT_AS3677_PATH := "/sys/devices/i2c-10/10-0040"
 SOMC_CFG_SENSORS_LIGHT_MAXRANGE := 12276
 SOMC_CFG_SENSORS_PROXIMITY_APDS9702 := yes
 
-# Bluetooth
-BOARD_HAVE_BLUETOOTH := true
-BOARD_HAVE_BLUETOOTH_QCOM := true
-BLUETOOTH_HCI_USE_MCT := true
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/sony/huashan/bluetooth
-
 # Power HAL
 TARGET_POWERHAL_VARIANT := qcom
 CM_POWERHAL_EXTENSION := qcom
@@ -139,7 +157,6 @@ BOARD_PROVIDES_LIBRIL := true
 BOARD_RIL_CLASS := ../../../device/sony/huashan/ril/
 
 # Healthd
-BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGER_SHOW_PERCENTAGE := true
 BACKLIGHT_PATH := /sys/devices/i2c-10/10-0040/leds/lcd-backlight1/brightness
 SECONDARY_BACKLIGHT_PATH := /sys/devices/i2c-10/10-0040/leds/lcd-backlight2/brightness
@@ -168,16 +185,6 @@ TARGET_DISPLAY_USE_RETIRE_FENCE := true
 NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
 OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 HAVE_ADRENO_SOURCE := false
-
-# Audio
-BOARD_USES_ALSA_AUDIO := true
-BOARD_USES_LEGACY_ALSA_AUDIO := true
-TARGET_USES_QCOM_COMPRESSED_AUDIO := true
-BOARD_HAVE_NEW_QCOM_CSDCLIENT := true
-BOARD_HAVE_CSD_FAST_CALL_SWITCH := true
-BOARD_USES_FLUENCE_INCALL := true
-BOARD_USES_SEPERATED_AUDIO_INPUT := true
-BOARD_USES_SEPERATED_VOICE_SPEAKER_MIC := true
 
 # FM Radio
 QCOM_FM_ENABLED := true
