c4d21d698846ec4976846976af563b6367089b2c
Adrian DC <AdrianDC@users.noreply.github.com>
2015-10-15 16:33:41 +0200

    Update full_huashan.mk

--- full_huashan.mk
+++ full_huashan.mk
@@ -31,6 +31,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 # Common Sony Resources
 $(call inherit-product, device/sony/common/resources.mk)
 
+# Build fingerprints
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C5303 BUILD_FINGERPRINT=Sony/C5303/C5303:4.3/12.1.A.1.207/Nvt_nw:user/release-keys PRIVATE_BUILD_DESC="C5303-user 4.3 JB-MR2-VISKAN-140318-1014 227 test-keys"
+
 PRODUCT_NAME := huashan
 PRODUCT_DEVICE := huashan
 PRODUCT_BRAND := Sony
