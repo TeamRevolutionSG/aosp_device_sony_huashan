9c5b70753ba8bf5deae06729bb68c3bcddbae332
Adrian DC <AdrianDC@users.noreply.github.com>
2015-10-15 20:42:31 +0200

    Update full_huashan.mk

--- full_huashan.mk
+++ full_huashan.mk
@@ -28,16 +28,13 @@ TARGET_SCREEN_WIDTH := 720
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-# Common Sony Resources
-$(call inherit-product, device/sony/common/resources.mk)
-
-# Build fingerprints
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C5303 BUILD_FINGERPRINT=Sony/C5303/C5303:4.3/12.1.A.1.207/Nvt_nw:user/release-keys PRIVATE_BUILD_DESC="C5303-user 4.3 JB-MR2-VISKAN-140318-1014 227 test-keys"
-
 PRODUCT_NAME := huashan
 PRODUCT_DEVICE := huashan
 PRODUCT_BRAND := Sony
 PRODUCT_MANUFACTURER := Sony
 PRODUCT_MODEL := Xperia SP
+
+TARGET_PREBUILT_KERNEL ?= kernel
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/$(TARGET_PREBUILT_KERNEL):kernel
 
 $(call inherit-product, device/sony/huashan/device.mk)
