2910aea9ceea5c996281306ae261e3b2e2e8edf5
Adrian DC <AdrianDC@users.noreply.github.com>
2015-10-17 14:26:36 +0200

    Update patch.sh

--- patches/patch.sh
+++ patches/patch.sh
@@ -1,3 +1,3 @@
 # Error 1 Patch
-rm ../../../hardware/qcom/display/msm8960/libcopybit/copybit_c2d.cpp
-mv copybit_c2d.cpp ../../../hardware/qcom/display/msm8960/libcopybit/copybit_c2d.cpp
+rm ../../../hardware/qcom/display/msm8960/libcopybit/copybit_c2d.cpp;
+cp ./copybit_c2d.cpp ../../../hardware/qcom/display/msm8960/libcopybit/copybit_c2d.cpp;
