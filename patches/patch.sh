d10d3307df299d14dde16392caa1d70ce42ed742
Firdaus Amin <furrydaus@gmail.com>
2015-10-17 20:48:08 +0800

    Update patch.sh

--- patches/patch.sh
+++ patches/patch.sh
@@ -1,3 +1,7 @@
 # Error 1 Patch
 rm ../../../../hardware/qcom/display/msm8960/libcopybit/copybit_c2d.cpp;
 cp ./copybit_c2d.cpp ../../../../hardware/qcom/display/msm8960/libcopybit/copybit_c2d.cpp;
+
+# Error 2 Patch
+cd ../../../../
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_hardware_qcom_media refs/changes/67/112267/1 && git cherry-pick FETCH_HEAD
